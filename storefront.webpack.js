a92cd1fb45aa67c99f2a281f381891b3a7613109
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-06-27 13:48:46 -0300

    chore: filter inside page filter

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -6,7 +6,9 @@ module.exports = () => ({
       './html/ProductCard.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductCard.html'),
       './js/ProductCard.js': path.resolve(__dirname, 'template/js/custom-js/js/ProductCard.js'),
       './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html'),
-      './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/js/TheProduct.js')
+      './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/js/TheProduct.js'),
+      './html/SearchEngine.html': path.resolve(__dirname, 'template/js/custom-js/html/SearchEngine.html'),
+      './js/SearchEngine.js': path.resolve(__dirname, 'template/js/custom-js/js/SearchEngine.js'),
     }
   }
 })
