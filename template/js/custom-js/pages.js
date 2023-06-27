3c245de1787ea43fa43d5017eff80aa24a248175
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-06-27 14:09:39 -0300

    chore: update layout

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,4 @@
 // Add your custom JavaScript for storefront pages here.
+$('body').on('click','.search-engine__aside-open, .search-engine__aside .card-header .close, .search-engine__toggles > button',function(){
+    $('body .search-engine__aside').toggleClass('active')
+})
