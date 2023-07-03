8bdb200cb42f23ff340f400b098e83dcb96d8354
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-07-03 17:02:44 -0300

    chore: un done category hidden

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -4,4 +4,3 @@ $('body').on('click','.search-engine__aside-open, .search-engine__aside .card-he
 })
 $('#search-engine-snap, #search-engine-load').wrap('<div class="container"></div>').wrap('<div class="row"></div>').wrap('<div class="col-md-9 col-12 offset-md-3"></div>');
 $('#search-engine-snap > article > .row > div').removeClass('col-lg-3');
-$('#c-645995294bd2e929c1f2d64f').replaceWith('<a href="/caixas-embalagens-para-doces-e-brigadeiro" id="c-645995294bd2e929c1f2d64f" class="menu__item"><span>Embalagem para Doces</span></a>')
